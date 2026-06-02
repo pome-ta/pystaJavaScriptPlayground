40a8c9452281c2ead19b629ad649f2fb9b1e17db
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-06-02 14:43:26 +0900

    tone.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,9 +1,20 @@
 import * as Tone from 'tone';
 
+const synth = new Tone.Synth().toDestination();
 
-console.log('fuha');
+const playButton = document.createElement('button');
+playButton.textContent = 'play';
+
+playButton.addEventListener('click', () => {
+  //play a middle 'C' for the duration of an 8th note
+  const now = Tone.now();
+  // trigger the attack immediately
+  synth.triggerAttack('C4', now);
+  // wait one second before triggering the release
+  synth.triggerRelease(now + 1);
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
-
+  document.body.appendChild(playButton);
 });
