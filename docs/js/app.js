8243a085cf47ef38b0f33079f36b77d4554cb015
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-06-23 16:41:02 +0900

    Quick Start

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,1 +1,16 @@
-console.log('hoge')
+console.log('hoge');
+
+import { createContainer } from 'almostnode';
+
+const { vfs, runtime, npm } = createContainer();
+
+// Write a file to the virtual filesystem
+vfs.writeFileSync(
+  '/hello.js',
+  `
+  console.log('Hello from almostnode!');
+`,
+);
+
+// Execute it
+runtime.runFile('/hello.js');
