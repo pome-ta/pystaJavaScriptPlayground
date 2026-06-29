8499437ffa40d0db35ebcad28eac28c5806de119
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-06-29 17:34:26 +0900

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,16 +5,22 @@ const compilerOptions = {
   target: ts.ScriptTarget.ES2022,
 };
 
-const fsMap = await tsvfs.createDefaultMapFromCDN(compilerOptions, ts.version, false, ts);
-fsMap.set('/main.ts', '');
+const fsMap = await tsvfs.createDefaultMapFromCDN(
+  compilerOptions,
+  ts.version,
+  false,
+  ts,
+);
+// 初期ファイルを空文字 ("") にすると `createVirtualTypeScriptEnvironment()` が
+// TS6053 を投げる既知の問題があるため、"\n" を初期値として登録
+fsMap.set('/main.ts', '\n');
 
 const system = tsvfs.createSystem(fsMap);
+const env = tsvfs.createVirtualTypeScriptEnvironment(
+  system,
+  ['/main.ts'],
+  ts,
+  compilerOptions,
+);
 
-console.log(ts.version);
-console.log(tsvfs);
-console.log(system.fileExists('/main.ts'));
-console.log(system.readFile('/main.ts'));
-
-const env = tsvfs.createVirtualTypeScriptEnvironment(system, ['/main.ts'], ts, compilerOptions);
-
-console.log(env)
+console.log(env);
