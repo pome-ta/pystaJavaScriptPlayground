363b0aae54a2586209fed128468fdf78ac16ada4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-07-12 19:30:14 +0900

    up

--- docs/js/sandboxes/sandbox.js
+++ docs/js/sandboxes/sandbox.js
@@ -1,10 +1,19 @@
+import * as Tone from "tone";
+
 import DomFactory from '../utils/domFactory.js';
 
 console.log(`[sandbox] top`);
 const context = new AudioContext();
+Tone.setContext(context)
 
 window.addEventListener('message', (e) => {
   console.log(e);
   console.log(context.state);
   context.resume();
+  const ctx = Tone.getContext();
+  console.log(ctx.state)
 });
+
+
+
+
