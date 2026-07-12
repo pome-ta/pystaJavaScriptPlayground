9cf956acc946040aaa18a67bcb54b66e4200a819
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-07-12 17:24:57 +0900

    up

--- docs/js/sandboxes/sandbox.js
+++ docs/js/sandboxes/sandbox.js
@@ -1,7 +1,10 @@
 import DomFactory from '../utils/domFactory.js';
 
 console.log(`[sandbox] top`);
+const context = new AudioContext();
 
 window.addEventListener('message', (e) => {
   console.log(e);
+  console.log(context.state);
+  context.resume();
 });
