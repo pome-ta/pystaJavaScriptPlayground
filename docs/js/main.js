aba3a79b101719a744d6bcdff77c926388b05ed5
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-07-13 15:09:27 +0900

    audio call

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,37 +1,133 @@
 import DomFactory from './utils/domFactory.js';
 
-const isInstance = true;
+// todo: MouseEvent TouchEvent wrapper
+const { touchBegan, touchMoved, touchEnded } = {
+  touchBegan:
+    typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
+  touchMoved:
+    typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
+  touchEnded:
+    typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
+};
+
+document.addEventListener(touchEnded, () => {
+  console.log(`--- ${Date.now()}: document`);
+  const auCtx = window.frames[0]?.auCtx;
+  if (auCtx) {
+    if (auCtx.state === 'suspended') {
+      auCtx.resume().then(() => {
+        console.log('AudioContext is now running');
+      });
+    }
+  }
+});
+
+const isInstance = false;
 
 const instanceMode = `const sketch = (p) => {
   const v = 360;
+  let osc, playing, freq, amp;
+  let musicalScale = [261.6, 311.1, 349.2, 370, 392, 466.2];
 
   p.setup = () => {
     // put setup code here
-    p.createCanvas(v, v);
+    const cnv = p.createCanvas(v, v);
     p.colorMode(p.HSL, v, 1, 1);
-    console.log(p);
+    // cnv.mousePressed(playOscillator);
+    // p.userStartAudio();
+    // console.log(p5.prototype.getAudioContext().state)
+    osc = new p5.Oscillator('sawtooth');
+
+    let s = p.second();
+    // console.log(s);
+    // console.log(s % musicalScale.length);
+    // osc.freq(440);
+    osc.freq(musicalScale[s % musicalScale.length]);
+    osc.amp(0.1);
+    osc.start();
   };
 
   p.draw = () => {
     // put drawing code here
     p.background(p.frameCount % v, 1, 0.5);
+    freq = musicalScale[p.floor(p.map(p.mouseX, 0, p.width, 0, 5))];
+
+    amp = p.constrain(p.map(p.mouseY, p.height, 0, 0, 1), 0, 1);
+    p.text('tap to play', 20, 20);
+    p.text('freq: ' + freq, 20, 40);
+    p.text('amp: ' + amp, 20, 60);
+
+    if (playing == true) {
+      osc.freq(freq);
+      osc.amp(amp);
+    }
   };
+
+  function playOscillator() {
+    // starting an oscillator on a user gesture will enable audio
+    // in browsers that have a strict autoplay policy.
+    osc.start();
+    playing = true;
+  }
 };
 
-new p5(sketch);`;
+new p5(sketch);
+`;
 
-const globalMode = `const v = 360;
+const globalMode = `
+
+// const v = 360;
+
+// function setup() {
+//   createCanvas(v, v);
+//   colorMode(HSL, v, 1, 1);
+// }
+
+// function draw() {
+//   background(frameCount % v, 1, 0.5);
+// }
+let osc, playing, freq, amp;
+let musicalScale = [261.6, 311.1, 349.2, 370, 392, 466.2]
 
 function setup() {
-  createCanvas(v, v);
-  colorMode(HSL, v, 1, 1);
+  let cnv = createCanvas(360, 360);
+  // cnv.mousePressed(playOscillator);
+  osc = new p5.Oscillator('sawtooth');
+  let s = second();
+  // console.log(s);
+  // console.log(s % musicalScale.length);
+  // osc.freq(440);
+  osc.freq(musicalScale[s % musicalScale.length]);
+  osc.amp(0.1);
+  osc.start();
+
 }
 
 function draw() {
-  background(frameCount % v, 1, 0.5);
-}`;
+  background(220)
+  freq = musicalScale[floor(map(mouseX, 0, width, 0, 5))];
+
+  amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
+  text('tap to play', 20, 20);
+  text('freq: ' + freq, 20, 40);
+  text('amp: ' + amp, 20, 60);
+
+  if (playing == true) {
+    osc.freq(freq);
+    osc.amp(amp);
+  }
+}
+
+function playOscillator() {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  osc.start();
+  playing = true;
+}
+`;
 
 const editor = DomFactory.create('textarea', {
+  setAttrs: { id: 'editor' },
   textContent: isInstance ? instanceMode : globalMode,
   setStyles: {
     width: '90%',
@@ -48,9 +144,14 @@ const reloadSketchHandleEvent = function (e) {
   this.targetSandbox = this.targetSandbox ? this.targetSandbox : e.target;
 
   if (e.type !== 'load') {
+    console.log(`--- ${Date.now()}: touchBegan`);
     this.targetSandbox.src = srcPath;
   }
-  this.targetSandbox.contentWindow.postMessage({ code: toStringDoc }, '*');
+
+  this.targetSandbox.contentWindow.postMessage(
+    { code: toStringDoc, isInstanceMode: isInstance },
+    '*',
+  );
 };
 
 /* --- iframe */
@@ -102,7 +203,7 @@ const callButton = DomFactory.create('button', {
   appendParent: wrapButton,
   addEventListeners: [
     {
-      type: 'click',
+      type: touchBegan,
       listener: {
         targetSandbox: sandbox,
         targetEditor: editor,
@@ -122,3 +223,4 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
   setLayout();
 });
+
--- docs/js/sandboxes/sandbox.js
+++ docs/js/sandboxes/sandbox.js
@@ -2,57 +2,24 @@ import DomFactory from '../utils/domFactory.js';
 
 console.log(`[sandbox] top`);
 
-function runSketch(code) {
-  if (window._p5Instance) {
-    window._p5Instance.remove();
-    window._p5Instance = null;
-  }
-
+function runSketch(code, isInstanceMode) {
   const script = DomFactory.create('script', {
     setAttrs: {
-      type: 'text/javascript',
+      type: isInstanceMode ? 'module' : 'text/javascript',
     },
-    // xxx: スコープを切る
-    textContent: `{
-      ${code}
-    }`,
+    // xxx: スコープを切る
+    textContent: isInstanceMode ? `${code}` : `{\n${code}\n}`,
     appendParent: document.body,
   });
 
-  if (window._p5Instance === null) {
-    try {
-      window._p5Instance = new p5();
-    } catch (e) {
-      console.log('Error: ' + e.message);
-    }
-  }
-
-  document.body.removeChild(script);
+  isInstanceMode ? null : new p5();
+  // document.body.removeChild(script);
 }
-
-// window.addEventListener('message', (e) => {
-//   console.log(e);
-//   const nowDate = new Date();
-//   console.log(nowDate.getSeconds() % 4);
-// });
 
 window.addEventListener('message', (e) => {
-  console.log('hogw');
   const sourceCode = e.data.code;
-  runSketch(sourceCode);
+  const isInstance = e.data.isInstanceMode;
+
+  runSketch(sourceCode, isInstance);
 });
 
-window._p5Instance = null;
-
-window.__p5 = window.p5;
-// delete window.p5;
-
-class p5 extends window.__p5 {
-  constructor(sketch, node) {
-    super(sketch, node);
-    window._p5Instance = this;
-  }
-}
-
-window.p5 = p5;
-delete window.__p5;
