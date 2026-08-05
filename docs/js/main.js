4bbe1296a9c15612269118ff00f716c83b5e62e6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-08-05 17:10:59 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,16 +10,15 @@ const { touchBegan, touchMoved, touchEnded } = {
     typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
 };
 
-const FFT_SIZE = 256;
+const FFT_SIZE = 1024;
 const ctx = new AudioContext();
 
-// アナライザーを生成
+
 const fft = ctx.createAnalyser();
-// フーリエ変換を行う分割数。2の乗数でなくてはならない
 fft.fftSize = FFT_SIZE;
-// 0~1の範囲でデータの動きの速さ 0だともっとも速く、1に近づくほど遅くなる
-fft.smoothingTimeConstant = 0.85;
-// オーディオの出力先を設定
+fft.smoothingTimeConstant = 0.8;
+
+
 fft.connect(ctx.destination);
 
 // audio 要素と紐付ける
@@ -29,11 +28,10 @@ nodeSource.connect(nodeAnalyser);
 
 
 
-/** 描画します */
 function loop() {
   window.requestAnimationFrame(loop);
 
-  // ・・・
+
   // ここに描画処理を書く
   console.log('l');
   
