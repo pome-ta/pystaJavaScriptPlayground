70e5ba8bee5998f8f5b136afe462c40c3538351e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-08-06 04:54:23 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,46 +2,59 @@ import DomFactory from './utils/domFactory.js';
 
 // todo: MouseEvent TouchEvent wrapper
 const { touchBegan, touchMoved, touchEnded } = {
-  touchBegan:
-    typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
-  touchMoved:
-    typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
-  touchEnded:
-    typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
+  touchBegan: typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
+  touchMoved: typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
+  touchEnded: typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
 };
 
-const FFT_SIZE = 1024;
 const ctx = new AudioContext();
 
+const mainOsc = ctx.createOscillator();
+mainOsc.type = 'sine';
+mainOsc.frequency.setValueAtTime(440, ctx.currentTime);
 
+
+
+const FFT_SIZE = 1024;
 const fft = ctx.createAnalyser();
 fft.fftSize = FFT_SIZE;
 fft.smoothingTimeConstant = 0.8;
 
-
-fft.connect(ctx.destination);
+//fft.connect(ctx.destination);
+mainOsc.connect(ctx.destination);
 
 // audio 要素と紐付ける
+/*
 const nodeSource = context.createMediaElementSource(audioElement);
 
 nodeSource.connect(nodeAnalyser);
-
-
-
+*/
 function loop() {
   window.requestAnimationFrame(loop);
 
-
   // ここに描画処理を書く
   console.log('l');
-  
 }
 
-
+document.addEventListener(
+  touchEnded,
+  () => {
+    console.log(ctx);
+    if (!ctx || ctx.state !== 'suspended') {
+      console.log('sus');
+      return;
+    }
+    ctx.resume().then(() => {
+      console.log('🔊: AudioContext is now running');
+      mainOsc.start();
+    });
+  },
+  //{ once: true },
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
-  loop();
-  
+  //loop();
 });
+
 
