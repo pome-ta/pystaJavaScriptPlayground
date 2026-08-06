b9f5bbbe49e7f03fdf177748eb788c31a72ec2b2
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-08-07 02:29:10 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,30 +6,26 @@ const mainOsc = ctx.createOscillator();
 mainOsc.type = 'sine';
 mainOsc.frequency.setValueAtTime(440, ctx.currentTime);
 
-const FFT_SIZE = 1024;
+const FFT_SIZE = 4096;
 const fft = ctx.createAnalyser();
 fft.fftSize = FFT_SIZE;
-fft.smoothingTimeConstant = 0.8;
+//fft.smoothingTimeConstant = 0.8;
+fft.smoothingTimeConstant = 0;
 
 const bufferLength = fft.frequencyBinCount;
 const dataArray = new Float32Array(bufferLength);
-
 
 fft.connect(ctx.destination);
 //mainOsc.connect(ctx.destination);
 mainOsc.connect(fft);
 mainOsc.start();
 
-
 /**
  * Convert decibels into gain.
  */
 function dbToGain(db) {
-	return Math.pow(10, db / 20);
+  return Math.pow(10, db / 20);
 }
-
-
-
 
 function loop() {
   window.requestAnimationFrame(loop);
@@ -37,14 +33,12 @@ function loop() {
   // ここに描画処理を書く
   //console.log('l');
   fft.getFloatFrequencyData(dataArray);
-  let spectrum = dataArray.map(v => dbToGain(v));
+  let spectrum = dataArray.map((v) => dbToGain(v));
   //console.log(dataArray)
   //console.log(Math.max(...dataArray));
   //console.log(dataArray)
   console.log(Math.max(...spectrum));
   //console.log(spectrum);
-
-  
 }
 
 const eventName = typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup';
