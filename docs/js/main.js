e093ef42622817182b585635e44ee5177d8755f6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-08-06 11:40:37 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,19 +1,10 @@
 import DomFactory from './utils/domFactory.js';
 
-// todo: MouseEvent TouchEvent wrapper
-const { touchBegan, touchMoved, touchEnded } = {
-  touchBegan: typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
-  touchMoved: typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
-  touchEnded: typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
-};
-
 const ctx = new AudioContext();
 
 const mainOsc = ctx.createOscillator();
 mainOsc.type = 'sine';
 mainOsc.frequency.setValueAtTime(440, ctx.currentTime);
-
-
 
 const FFT_SIZE = 1024;
 const fft = ctx.createAnalyser();
@@ -22,7 +13,7 @@ fft.smoothingTimeConstant = 0.8;
 
 //fft.connect(ctx.destination);
 mainOsc.connect(ctx.destination);
-
+mainOsc.start();
 // audio 要素と紐付ける
 /*
 const nodeSource = context.createMediaElementSource(audioElement);
@@ -36,25 +27,15 @@ function loop() {
   console.log('l');
 }
 
-document.addEventListener(
-  touchEnded,
-  () => {
-    console.log(ctx);
-    if (!ctx || ctx.state !== 'suspended') {
-      console.log('sus');
-      return;
-    }
-    ctx.resume().then(() => {
-      console.log('🔊: AudioContext is now running');
-      mainOsc.start();
-    });
-  },
-  //{ once: true },
-);
+const eventName = typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup';
+document.addEventListener(eventName, initAudioContext);
+function initAudioContext() {
+  document.removeEventListener(eventName, initAudioContext);
+  // wake up AudioContext
+  ctx.resume();
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
   //loop();
 });
-
-
